cde2887d7dc44d585ff3894f90236308b0809870
justin <t110598402@ntut.org.tw>
2023-02-08 15:04:18 +0800

    finished chatting room UI

--- src/components/AIChatComponent.tsx
+++ src/components/AIChatComponent.tsx
@@ -1,26 +1,30 @@
 import './AIChatComponent.css';
 import { Configuration, OpenAIApi } from "openai";
 import React, { useState } from 'react';
-import { IonBackButton, IonButton, IonButtons, IonCard, IonContent, IonHeader, IonItem, IonLabel, IonTextarea, IonTitle, IonToolbar, IonIcon } from '@ionic/react';
-import { person, logoReact } from 'ionicons/icons';
+import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonTextarea, IonTitle, IonToolbar, IonList, IonAvatar, IonText } from '@ionic/react';
 import config from '../config';
 
-function AIChatComponent() {
-  const [currentInputChat, setCurrentInputChat] = useState('');
-  const [conversationHistory, setConversationHistory] = useState('');
-  const [inputChatHistory, setinputChatHistory] = useState<string[]>([]);
-  const [outputChatHistory, setOutputChatHistory] = useState<string[]>([]);
+interface ChatMessage {
+  message: string;
+  sender: string;
+  time: string;
+}
 
-  const handleAndGetConversationHistory=()=>{
-    let conversation = "";
-    for (let i=0; i<outputChatHistory.length; i++){
-      conversation = conversation + "Q:" + inputChatHistory[i] + "\n";
-      conversation = conversation + "A:" + outputChatHistory[i] + "\n";
+function AIChatComponent() {
+  const [currentInputMsg, setCurrentInputMsg] = useState('');
+  const [chatHistory, setChatHistory] = useState<ChatMessage[]>([]);
+
+  const getPromptByChatHistory=()=>{
+    let chat = "";
+    for (var history of chatHistory){
+      if(history.sender === 'Q'){
+        chat = chat + "Q: " + history.message + "\n";
+      }else{
+        chat = chat + "A: " + history.message + "\n";
+      }
     }
-    conversation = conversation + "Q:" + currentInputChat;
-    setConversationHistory(conversation);
-    console.log(conversation);
-    return conversation;
+    console.log(chat);
+    return chat;
   }
 
   const configuration = new Configuration({
@@ -39,16 +43,27 @@ function AIChatComponent() {
     return completion.data.choices[0].text!.trim();
   }
 
-  const onSendButtonClick =async ()=>{
-    inputChatHistory.push(currentInputChat);
-    const result = await Promise.resolve(getAIResult(handleAndGetConversationHistory()));
-    outputChatHistory?.push(result);
-    const newOutputChatHistory = Object.assign([], outputChatHistory);
-    setOutputChatHistory(newOutputChatHistory);
-    const newInputChatHistory = Object.assign([], inputChatHistory);
-    setinputChatHistory(newInputChatHistory);
-    setCurrentInputChat('');
-  }
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();    
+    chatHistory.push({
+      message: currentInputMsg,
+      sender: 'Q',
+      time: new Date().toLocaleTimeString()
+    });
+    setChatHistory(Object.assign([], chatHistory));
+    const prompt = getPromptByChatHistory();
+    setCurrentInputMsg('');
+    let aiMsg = await Promise.resolve(getAIResult(prompt));
+    if (aiMsg.startsWith("A: ")) {
+      aiMsg = aiMsg.substring(3);
+    }
+    chatHistory.push({
+      message: aiMsg,
+      sender: 'A',
+      time: new Date().toLocaleTimeString()
+    });
+    setChatHistory(Object.assign([], chatHistory));
+  };
 
   return (
     <>
@@ -59,25 +74,27 @@ function AIChatComponent() {
         </IonButtons>
         <IonTitle>AIChat</IonTitle>
         </IonToolbar>
-      </IonHeader>      
-      <IonContent className="ion-padding">
-        <IonItem>
-          <IonLabel position="floating">Write down here to chat with AI?</IonLabel>
-          <IonTextarea placeholder="Enter text" autoGrow={true} value={currentInputChat} onIonChange={e => setCurrentInputChat(e.detail.value!)}/>
-          <IonButton onClick={ () => onSendButtonClick()} color="light" shape="round">Send</IonButton><br></br>
-        </IonItem>
-        {Array.from({length: outputChatHistory?.length}, (_, i) => (
-          <div key={i}>
-            <IonCard class="ion-padding">
-              <IonIcon icon={person}></IonIcon>:          
-              <p>{inputChatHistory[i]}</p>
-            </IonCard>
-            <IonCard class="ion-padding">
-              <IonIcon icon={logoReact}></IonIcon>:
-              <p>{outputChatHistory[i]}</p>
-            </IonCard>
-          </div>
-        ))}
+      </IonHeader>
+      <IonContent>
+        <IonList className="chat-history">
+          {chatHistory.map((item, index) => (
+            <IonItem key={index} className={item.sender === 'Q' ? 'own-message' : 'other-message'}>
+              <IonAvatar slot={item.sender === 'Q' ? 'end' : 'start'}>
+                <img src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+              </IonAvatar>
+              <IonLabel className="message-content">
+                <IonText>
+                    <p>{item.message}</p>
+                </IonText>
+                <p className="message-time">{item.time}</p>
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
+        <form onSubmit={handleSubmit} className="chat-form">
+          <IonTextarea placeholder="Enter message..." autoGrow={true} value={currentInputMsg} onIonChange={e => setCurrentInputMsg(e.detail.value!)}/>
+          <IonButton type="submit" shape='round' color='light'>Send</IonButton>
+        </form>
       </IonContent>
     </>
   );

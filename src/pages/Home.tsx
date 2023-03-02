a2c89b8f024b7fb6415e7aab5696cda30c3c10fc
justin <t110598402@ntut.org.tw>
2023-03-02 17:30:35 +0800

    added apikey input function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,15 @@
-import { IonButton, IonContent, IonHeader, IonNavLink, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonModal, IonNavLink, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useRef } from 'react';
 import AIChatComponent from '../components/AIChatComponent'
 import GenerateImageComponent from '../components/GenerateImageComponent';
 
 const Home: React.FC = () => {
+  const modal = useRef<HTMLIonModalElement>(null);
+  const apiInputRef = useRef<HTMLIonInputElement>(null);
+  function dismiss() {
+    console.log('clicked')
+    modal.current?.dismiss();
+  }
   return (
     <IonPage>
       <IonHeader>
@@ -18,10 +25,27 @@ const Home: React.FC = () => {
               </IonNavLink><br></br><br></br>
               <IonNavLink routerDirection="forward" component={() => <GenerateImageComponent />}>
                 <IonButton shape="round" class="card-btn">Generate Image</IonButton>
-              </IonNavLink>
+              </IonNavLink><br></br><br></br>
+              <IonButton id="open-api-key-input" shape="round" class="card-btn">Enter API Key</IonButton>
             </div>
           </div>
-        </IonContent>
+        </IonContent>        
+        <IonModal id="api-key-modal" ref={modal} trigger="open-api-key-input">
+          <IonContent>
+            <IonToolbar>
+              <IonTitle>API KEY</IonTitle>
+              <IonButtons slot="end">
+                <IonButton onClick={() => {console.log('clicked')}}>
+                  Comfirm
+                </IonButton>
+              </IonButtons>
+            </IonToolbar>
+            <IonItem>
+              <IonLabel>Enter Your API Key:</IonLabel>
+              <IonInput ref={apiInputRef} placeholder="Your API Key" />
+            </IonItem>
+          </IonContent>
+        </IonModal>
     </IonPage>
   );
 };
